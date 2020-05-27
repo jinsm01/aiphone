8311ccec15aa360190292a61c10be87ff35c4836
jsm <18842605479@163.com>
2020-05-27 15:13:26 +0800

    数据驱动

--- UI测试框架/tase_case/test_main.py
+++ UI测试框架/tase_case/test_main.py
@@ -3,10 +3,16 @@
 # @Time    : 2020/5/27 14:01
 # @Author  : shaonianlang
 # @File    : test_main.py
+import pytest
+import yaml
+
 from UI测试框架.page.app import App
 
 class TestMain:
 
-    def test_main(self):
-        app = App()
-        app.start().main().goto_search()
+    @pytest.mark.parametrize('value1, value2', yaml.safe_load(open("../tase_case/test_main.yaml")))
+    def test_main(self, value1, value2):
+        # app = App()
+        # app.start().main().goto_search()
+        print(value2)
+        print(value1)
